ce6c2661f18ffb56e67c41a396228696e2fdf71b
Anna-Tian <37281865+Anna-Tian@users.noreply.github.com>
2023-03-15 23:34:48 +1100

    setup buzzer

--- safetyLesson.ino
+++ safetyLesson.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+#define BUZZER_PIN 6
 
+void setup()
+{
+  pinMode(BUZZER_PIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(1000);
+  digitalWrite(BUZZER_PIN, LOW);
+  delay(1000);
 }
