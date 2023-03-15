4cc9f2e35e58858afeec86cf853573d4f9b1eb15
Anna-Tian <37281865+Anna-Tian@users.noreply.github.com>
2023-03-15 23:39:44 +1100

    setup ultrasonic distance sensor

--- safetyLesson.ino
+++ safetyLesson.ino
@@ -1,14 +1,33 @@
+#include <Ultrasonic.h>
+
+
+Ultrasonic ultrasonic(7);
 #define BUZZER_PIN 6
+
 
 void setup()
 {
-  pinMode(BUZZER_PIN, OUTPUT);
+  Serial.begin(9600);
+  // pinMode(BUZZER_PIN, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(BUZZER_PIN, HIGH);
-  delay(1000);
-  digitalWrite(BUZZER_PIN, LOW);
-  delay(1000);
+  long RangeInInches;
+  long RangeInCentimeters;
+
+  Serial.println("The distance to obstacles in front is: ");
+  RangeInInches = ultrasonic.MeasureInInches();
+  Serial.print(RangeInInches);//0~157 inches
+  Serial.println(" inch");
+  delay(250);
+
+  RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+  Serial.print(RangeInCentimeters);//0~400cm
+  Serial.println(" cm");
+  delay(250);
+  // digitalWrite(BUZZER_PIN, HIGH);
+  // delay(1000);
+  // digitalWrite(BUZZER_PIN, LOW);
+  // delay(1000);
 }
