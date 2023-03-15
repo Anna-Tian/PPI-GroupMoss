5225c1b4dae90e08b865c835f5488f4ac14f8da7
Anna-Tian <37281865+Anna-Tian@users.noreply.github.com>
2023-03-16 03:28:48 +1100

    buzzer and vibrator work simultaneously

--- safetyLesson.ino
+++ safetyLesson.ino
@@ -1,3 +1,5 @@
+#include <Metro.h>
+
 #include <Ultrasonic.h>
 
 #include <Adafruit_NeoPixel.h>  // LED ring
@@ -9,31 +11,19 @@ Ultrasonic ultrasonic(7);
 
 // ---------------- Buzzer ----------------
 #define Buzzer_PIN 3
-int buzzerLength = 4;
-char buzzerNotes[] = "gec ";
-int buzzerBeats[] = {1, 1, 2};
-int buzzerTempo = 300;
-// ---------------- Buzzer end ----------------
+bool buzzerState = false;
+Metro buzzerMetro = Metro(500); //buzzer interval in millis
 
+// ---------------- Vibrator ----------------
 #define Vibration_PIN 2
+bool vibratorState = false;
+Metro vibratorMetro = Metro(600); //vibrator interval in millis
 
 // ---------------- LED ring ----------------
 #define LED_PIN     6
-// How many NeoPixels are attached to the Arduino?
-#define LED_COUNT  60
-// NeoPixel brightness, 0 (min) to 255 (max)
+#define LED_COUNT  60 // How many NeoPixels are attached to the Arduino?
 #define BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
-// Declare our NeoPixel strip object:
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
-// Argument 1 = Number of pixels in NeoPixel strip
-// Argument 2 = Arduino pin number (most are valid)
-// Argument 3 = Pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-// ---------------- LED ring end ----------------
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800); // Declare our NeoPixel strip object:
 
 void setup()
 {
@@ -45,43 +35,32 @@ void setup()
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(BRIGHTNESS);
-  // ---------------- LED ring end ----------------
 }
 
 void loop()
 {
-  //---------------- Ultrasonic distance sensor ----------------
-  long ultrasonicDistance;
-  ultrasonicDistance = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+  long ultrasonicDistance = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+  Serial.println((String)ultrasonicDistance + " cm");
   
+
   if (ultrasonicDistance < 11) {
     // ---------------- Buzzer ----------------
-    for(int i = 0; i < buzzerLength; i++) {
-      if(buzzerNotes[i] != ' ') {
-        playBuzzer(buzzerNotes[i], buzzerBeats[i] * buzzerTempo);
-      }
-        delay(buzzerTempo / 2);    /* delay between notes */
+    if(buzzerMetro.check()) {
+      buzzerState = !buzzerState;
+      digitalWrite(Buzzer_PIN, buzzerState);
     }
-    // ---------------- Buzzer end ----------------
 
     // ---------------- Vibrator ----------------
-    playVibration(buzzerTempo);
-    // ---------------- Vibrator end----------------
-
-    Serial.println((String)"Stay Away! It's too close: " + ultrasonicDistance + " cm");
+    if(vibratorMetro.check()) {
+      vibratorState = !vibratorState;
+      digitalWrite(Vibration_PIN, vibratorState);
+    }
   } else {
-    Serial.print(ultrasonicDistance);//0~400cm
-    Serial.println(" cm");
+    digitalWrite(Buzzer_PIN, LOW);
+    digitalWrite(Vibration_PIN, LOW);
   }
-  //---------------- Ultrasonic distance sensor end ----------------
 
-  // ---------------- Vibrator ----------------
-  // digitalWrite(Vibration_PIN, HIGH);
-  // delay(1000);
-
-  // digitalWrite(Vibration_PIN, LOW);
-  // delay(1000);
-  // ---------------- Vibrator end----------------
+  // Serial.println((String)"Stay Away! It's too close: " + ultrasonicDistance + " cm");
 
   // ringFlesh(5);
 }
--- vibrationMotor.ino
+++
@@ -1,6 +0,0 @@
-void playVibration(int duration) {
-  digitalWrite(Vibration_PIN, HIGH);
-  delay(duration);
-  digitalWrite(Vibration_PIN, LOW);
-  delay(duration);
-}
