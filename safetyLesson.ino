3f4eba2a024ffb06e963dc292fdaa5bde993a80e
Anna-Tian <37281865+Anna-Tian@users.noreply.github.com>
2023-03-16 00:24:55 +1100

    setup RGB LED Ring

--- safetyLesson.ino
+++ safetyLesson.ino
@@ -1,14 +1,39 @@
 #include <Ultrasonic.h>
 
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
 
 Ultrasonic ultrasonic(7);
-#define BUZZER_PIN 6
+#define BUZZER_PIN 3
 
+// ---------------- LED ring ----------------
+#define LED_PIN     6
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT  60
+// NeoPixel brightness, 0 (min) to 255 (max)
+#define BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+// ---------------- LED ring end ----------------
 
 void setup()
 {
   Serial.begin(9600);
   // pinMode(BUZZER_PIN, OUTPUT);
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(BRIGHTNESS);
 }
 
 void loop()
@@ -30,4 +55,21 @@ void loop()
   // delay(1000);
   // digitalWrite(BUZZER_PIN, LOW);
   // delay(1000);
+
+  ringFlesh(5);
+}
+
+void ringFlesh(uint8_t wait) {
+  for(int j=0; j<256; j++) { // Ramp up from 0 to 255
+    // Fill entire strip with white at gamma-corrected brightness level 'j':
+    strip.fill(strip.Color(0, 0, 0, strip.gamma8(j)));
+    strip.show();
+    delay(wait);
+  }
+
+  for(int j=255; j>=0; j--) { // Ramp down from 255 to 0
+    strip.fill(strip.Color(0, 0, 0, strip.gamma8(j)));
+    strip.show();
+    delay(wait);
+  }
 }
