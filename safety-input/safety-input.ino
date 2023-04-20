66b5e48044a4855aa818ee2c5a16a7baea50be4a
Anna-Tian <37281865+Anna-Tian@users.noreply.github.com>
2023-04-21 01:26:43 +1000

    change ultrasonic distance to time-of-flight distance

--- safety-input/safety-input.ino
+++ safety-input/safety-input.ino
@@ -1,21 +1,49 @@
 #include <SoftwareSerial.h>
+#include "Seeed_vl53l0x.h"
+Seeed_vl53l0x VL53L0X;
 
-#include <Ultrasonic.h>
-
-Ultrasonic ultrasonic(5);
 SoftwareSerial mySerial(2,3);
 char distance[10];
+
 void setup() {
-  Serial.begin(19200);
+  VL53L0X_Error Status = VL53L0X_ERROR_NONE;
+  Serial.begin(9600);
+  Status = VL53L0X.VL53L0X_common_init();
+  if (VL53L0X_ERROR_NONE != Status) {
+      Serial.println("start vl53l0x mesurement failed!");
+      VL53L0X.print_pal_error(Status);
+      while (1);
+  }
+
+  VL53L0X.VL53L0X_long_distance_ranging_init();
+
+  if (VL53L0X_ERROR_NONE != Status) {
+      Serial.println("start vl53l0x mesurement failed!");
+      VL53L0X.print_pal_error(Status);
+      while (1);
+  }
   mySerial.begin(9600);
   Serial.println("Test begin");
+  VL53L0X.VL53L0X_long_distance_ranging_init();
 }
 
 void loop() {
-  int ultrasonicDistance = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
-  Serial.println((String)"Stay Away! It's too close: " + ultrasonicDistance + " cm");
-  sprintf(distance,"%i\r",ultrasonicDistance);
-  mySerial.write(distance);
-  mySerial.write("\n");
+  VL53L0X_RangingMeasurementData_t RangingMeasurementData;
+  VL53L0X_Error Status = VL53L0X_ERROR_NONE;
+
+  memset(&RangingMeasurementData, 0, sizeof(VL53L0X_RangingMeasurementData_t));
+  Status = VL53L0X.PerformSingleRangingMeasurement(&RangingMeasurementData);
+  if (VL53L0X_ERROR_NONE == Status) {
+      Serial.print("Measured distance:");
+      Serial.print(RangingMeasurementData.RangeMilliMeter);
+      Serial.println(" mm");
+      sprintf(distance,"%i\r",RangingMeasurementData.RangeMilliMeter);
+      mySerial.write(distance);
+      mySerial.write("\n");
+  } else {
+      Serial.print("mesurement failed !! Status code =");
+      Serial.println(Status);
+  }
+  
   delay(500);
 }
--- safety-output/safety-output.ino
+++ safety-output/safety-output.ino
@@ -21,15 +21,6 @@ Metro buzzerMetro = Metro(600); // buzzer interval in millis
 bool vibratorState = false;
 Metro vibratorMetro = Metro(300); // vibrator interval in millis
 
-// // ---------------- LED ring ----------------
-// #define LED_Ring_PIN 2
-// #define LED_Ring_Count  60 // How many NeoPixels are attached to the Arduino?
-// #define LED_Ring_Brightness 50 // Set LED_Ring_Brightness to about 1/5 (max = 255)
-// Adafruit_NeoPixel strip(LED_Ring_Count, LED_Ring_PIN, NEO_GRBW + NEO_KHZ800); // Declare our NeoPixel strip object:
-
-// ---------------- LED socket kit ----------------
-#define LED_Kit_PIN 4
-
 // ---------------- LED chainable RGB ----------------
 #define LED_RGB_Count 5
 ChainableLED leds(7, 8, LED_RGB_Count);
@@ -40,7 +31,7 @@ bool isPressed = false;
 
 void setup()
 {
-  Serial.begin(19200);
+  Serial.begin(9600);
   mySerial.begin(9600);
 
   pinMode(Buzzer_PIN, OUTPUT);
@@ -49,24 +40,17 @@ void setup()
   pinMode(Vibration_PIN, OUTPUT );
   digitalWrite(Vibration_PIN, 0);
 
-  pinMode(LED_Kit_PIN, OUTPUT);
   leds.init();
 
   pinMode(Button_PIN, INPUT);
   Serial.println("Test begin");
-  // // ---------------- LED ring ----------------
-  // strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  // strip.show();            // Turn OFF all pixels ASAP
-  // strip.setLED_Ring_Brightness(LED_Ring_Brightness);
 }
 
 void loop()
 {
   if (mySerial.available()) {
-    int ultrasonicDistance = mySerial.readStringUntil('\n').toInt();
-    if (ultrasonicDistance < 15 && ultrasonicDistance !=0 && !isPressed) {
-      // Serial.println((String)"Stay Away! It's too close: " + ultrasonicDistance + " cm");
-      
+    int distance = mySerial.readStringUntil('\n').toInt();
+    if (distance < 300 && distance !=0 && !isPressed) {
       // ---------------- Buzzer ----------------
       if(buzzerMetro.check()) {
         buzzerState = !buzzerState;
@@ -78,14 +62,13 @@ void loop()
         digitalWrite(Vibration_PIN, vibratorState);
       }
       // ---------------- LED ----------------
-      digitalWrite(LED_Kit_PIN, HIGH); 
       leds.setColorRGB(0, 255, 0, 0);
 
       // ---------------- press button to stop playing ----------------
       if (digitalRead(Button_PIN) == HIGH) {
         isPressed = true;
       }
-    } else if (ultrasonicDistance < 30 && ultrasonicDistance !=0 && isPressed) {
+    } else if (distance < 300 && distance !=0 && isPressed) {
       leds.setColorRGB(0, 0, 0, 255);
       stopPlaying();
     } else {
@@ -94,19 +77,9 @@ void loop()
       stopPlaying();
     }
   }
-
-
-  //   // ---------------- LED ring ----------------
-  //   strip.fill(strip.Color(0, 0, 0, 255));
-  //   strip.show();
-
-    
-  //   strip.fill(strip.Color(0, 0, 0, 0));
-  //   strip.show();
 }
 
 void stopPlaying() {
-  digitalWrite(LED_Kit_PIN, LOW); 
   digitalWrite(Buzzer_PIN, false);
   digitalWrite(Vibration_PIN, false);
 }
