be2ad7db8af717065d4564d9b0180824ac3dcc07
Anna-Tian <37281865+Anna-Tian@users.noreply.github.com>
2023-04-21 01:47:34 +1000

    add infrared temperature sensor

--- safety-input/safety-input.ino
+++ safety-input/safety-input.ino
@@ -1,9 +1,18 @@
+// Bluetooth
 #include <SoftwareSerial.h>
+
+// time-of-flight distance
 #include "Seeed_vl53l0x.h"
 Seeed_vl53l0x VL53L0X;
 
+// infrared temperature 
+#include <Wire.h>
+#include "WaveShare_MLX90614.h"
+
 SoftwareSerial mySerial(2,3);
 char distance[10];
+WaveShare_MLX90614   MLX90614 = WaveShare_MLX90614();
+
 
 void setup() {
   VL53L0X_Error Status = VL53L0X_ERROR_NONE;
@@ -25,6 +34,7 @@ void setup() {
   mySerial.begin(9600);
   Serial.println("Test begin");
   VL53L0X.VL53L0X_long_distance_ranging_init();
+  MLX90614.begin();
 }
 
 void loop() {
@@ -34,9 +44,9 @@ void loop() {
   memset(&RangingMeasurementData, 0, sizeof(VL53L0X_RangingMeasurementData_t));
   Status = VL53L0X.PerformSingleRangingMeasurement(&RangingMeasurementData);
   if (VL53L0X_ERROR_NONE == Status) {
-      Serial.print("Measured distance:");
-      Serial.print(RangingMeasurementData.RangeMilliMeter);
-      Serial.println(" mm");
+      // Serial.print("Measured distance:");
+      // Serial.print(RangingMeasurementData.RangeMilliMeter);
+      // Serial.println(" mm");
       sprintf(distance,"%i\r",RangingMeasurementData.RangeMilliMeter);
       mySerial.write(distance);
       mySerial.write("\n");
@@ -44,6 +54,9 @@ void loop() {
       Serial.print("mesurement failed !! Status code =");
       Serial.println(Status);
   }
+
+  Serial.print("Ambient Temp: "); Serial.print(MLX90614.readAmbientTemp()); Serial.print(" C");
+  Serial.print(" \tObject Temp: "); Serial.print(MLX90614.readObjectTemp()); Serial.println(" C");
   
   delay(500);
 }
