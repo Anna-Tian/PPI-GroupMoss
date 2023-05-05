7ec920eb373a5f5000d2f361fc9b2e16422670ba
Anna-Tian <37281865+Anna-Tian@users.noreply.github.com>
2023-05-05 19:01:45 +1000

    detect whether obstacle is inanimate or human via infrared temperature sensor

--- safety-output/safety-output.ino
+++ safety-output/safety-output.ino
@@ -30,6 +30,9 @@ ChainableLED leds(7, 8, LED_RGB_Count);
 #define Button_PIN 5
 bool isPressed = false;
 
+float initialObjectTemp = 0;
+float prevObjectTemp = 0; 
+
 void setup()
 {
   Serial.begin(9600);
@@ -51,52 +54,75 @@ void setup()
 void loop()
 {
   if (mySerial.available()) {
-    int distance = mySerial.readStringUntil('\n').toInt();
-    if (distance < 1000 && distance !=0 && !isPressed) {
-      buzzerDuration = 300; // Set buzzer duration to normal
+    String receivedMessage = mySerial.readStringUntil('\n');
+    int separatorIndex = receivedMessage.indexOf(',');
 
-      // ---------------- Buzzer ----------------
-      if(buzzerMetro.check()) {
-        buzzerState = !buzzerState;
-        if(buzzerState) {
-          digitalWrite(Buzzer_PIN, HIGH); // turn on the buzzer
-          buzzerStartTime = millis(); // record the start time of the buzzer
-        } else {
-          digitalWrite(Buzzer_PIN, LOW); // turn off the buzzer
+    // Extract the distance value from the received message
+    String distanceStr = receivedMessage.substring(receivedMessage.indexOf(':') + 2, separatorIndex);
+    int distance = distanceStr.toInt();
+
+    // Extract the object temperature value from the received message
+    String objectTempStr = receivedMessage.substring(receivedMessage.lastIndexOf(':') + 2, receivedMessage.length() - 1);
+    float objectTemp = objectTempStr.toFloat();
+
+    if (initialObjectTemp == 0) {
+      initialObjectTemp = objectTemp; // set initial background environment temperature value
+    }
+    
+    Serial.println(abs(initialObjectTemp - prevObjectTemp));
+
+    if (abs(initialObjectTemp - prevObjectTemp) >= 2.5) {
+      if (distance < 1000 && distance !=0 && !isPressed) {
+        buzzerDuration = 300; // Set buzzer duration to normal
+
+        // ---------------- Buzzer ----------------
+        if(buzzerMetro.check()) {
+          buzzerState = !buzzerState;
+          if(buzzerState) {
+            digitalWrite(Buzzer_PIN, HIGH); // turn on the buzzer
+            buzzerStartTime = millis(); // record the start time of the buzzer
+          } else {
+            digitalWrite(Buzzer_PIN, LOW); // turn off the buzzer
+          }
         }
-      }
-      // ---------------- Vibrator ----------------
-      if(vibratorMetro.check()) {
-        vibratorState = !vibratorState;
-        digitalWrite(Vibration_PIN, vibratorState);
-      }
-      // ---------------- LED ----------------
-      leds.setColorRGB(0, 255, 0, 0); // red color
-
-      // ---------------- press button to stop playing ----------------
-      if (digitalRead(Button_PIN) == HIGH) {
-        isPressed = true;
-      }
-    } else if (distance < 1000 && distance !=0 && isPressed) {
-      buzzerDuration = longBuzzerDuration; // Set buzzer duration to shorter duration
-
-      // ---------------- Buzzer ----------------
-      if(buzzerMetro.check()) {
-        buzzerState = !buzzerState;
-        if(buzzerState) {
-          digitalWrite(Buzzer_PIN, HIGH); // turn on the buzzer
-          buzzerStartTime = millis(); // record the start time of the buzzer
-        } else {
-          digitalWrite(Buzzer_PIN, LOW); // turn off the buzzer
+        // ---------------- Vibrator ----------------
+        if(vibratorMetro.check()) {
+          vibratorState = !vibratorState;
+          digitalWrite(Vibration_PIN, vibratorState);
         }
+        // ---------------- LED ----------------
+        leds.setColorRGB(0, 255, 0, 0); // red color
+
+        // ---------------- press button to stop playing ----------------
+        if (digitalRead(Button_PIN) == HIGH) {
+          isPressed = true;
+        }
+      } else if (distance < 1000 && distance !=0 && isPressed) {
+        buzzerDuration = longBuzzerDuration; // Set buzzer duration to shorter duration
+
+        // ---------------- Buzzer ----------------
+        if(buzzerMetro.check()) {
+          buzzerState = !buzzerState;
+          if(buzzerState) {
+            digitalWrite(Buzzer_PIN, HIGH); // turn on the buzzer
+            buzzerStartTime = millis(); // record the start time of the buzzer
+          } else {
+            digitalWrite(Buzzer_PIN, LOW); // turn off the buzzer
+          }
+        }
+        leds.setColorRGB(0, 204, 102, 0); // yellow color
+        digitalWrite(Vibration_PIN, false);
+      } else {
+        isPressed = false;
+        leds.setColorRGB(0, 0, 255, 0); //green color
+        stopPlaying();
       }
-      leds.setColorRGB(0, 204, 102, 0); // yellow color
-      digitalWrite(Vibration_PIN, false);
     } else {
       isPressed = false;
       leds.setColorRGB(0, 0, 255, 0); //green color
       stopPlaying();
     }
+    prevObjectTemp = objectTemp;
   }
 
   // check if the buzzer has been on for longer than the desired duration
