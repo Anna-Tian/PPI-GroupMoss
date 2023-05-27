5e427712d31cc93f227b2c71840d009403b30b74
Anna-Tian <37281865+Anna-Tian@users.noreply.github.com>
2023-05-27 20:25:46 +1000

    closer distance will change the light and buzzer, click button will stop buzzer and vibration

--- safety-output/safety-output.ino
+++ safety-output/safety-output.ino
@@ -1,26 +1,26 @@
 #include <ChainableLED.h>
 #include <SoftwareSerial.h>
-#include <Metro.h> // make multiple output run at the same time
+#include <Metro.h>              // make multiple output run at the same time
 #include <Adafruit_NeoPixel.h>  // LED ring
 
 #ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h>  // Required for 16 MHz Adafruit Trinket
 #endif
 
-SoftwareSerial mySerial(2,3);
+SoftwareSerial mySerial(2, 3);
 
 // ---------------- Buzzer ----------------
 #define Buzzer_PIN 3
 bool buzzerState = false;
-Metro buzzerMetro = Metro(600); // buzzer interval in millis, increased to 200ms
+Metro buzzerMetro = Metro(600);  // buzzer interval in millis, increased to 200ms
 unsigned long buzzerStartTime = 0;
-unsigned long buzzerDuration = 300; // buzzer duration in milliseconds
-unsigned long longBuzzerDuration = 600; // longer buzzer duration in milliseconds
+unsigned long buzzerDuration = 300;      // buzzer duration in milliseconds
+unsigned long longBuzzerDuration = 600;  // longer buzzer duration in milliseconds
 
 // ---------------- Vibrator ----------------
 #define Vibration_PIN 6
 bool vibratorState = false;
-Metro vibratorMetro = Metro(300); // vibrator interval in millis
+Metro vibratorMetro = Metro(300);  // vibrator interval in millis
 
 // ---------------- LED chainable RGB ----------------
 #define LED_RGB_Count 5
@@ -31,28 +31,25 @@ ChainableLED leds(7, 8, LED_RGB_Count);
 bool isPressed = false;
 
 float initialObjectTemp = 0;
-float prevObjectTemp = 0; 
+float prevObjectTemp = 0;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
 
   pinMode(Buzzer_PIN, OUTPUT);
   digitalWrite(Buzzer_PIN, 0);
 
-  pinMode(Vibration_PIN, OUTPUT );
+  pinMode(Vibration_PIN, OUTPUT);
   digitalWrite(Vibration_PIN, 0);
 
   leds.init();
 
-  // pinMode(Button_PIN, INPUT);
   pinMode(Button_PIN, INPUT_PULLUP);
   Serial.println("Test begin");
 }
 
-void loop()
-{
+void loop() {
   if (mySerial.available()) {
     String receivedMessage = mySerial.readStringUntil('\n');
     int separatorIndex = receivedMessage.indexOf(',');
@@ -66,73 +63,81 @@ void loop()
     float objectTemp = objectTempStr.toFloat();
 
     if (initialObjectTemp == 0) {
-      initialObjectTemp = objectTemp; // set initial background environment temperature value
+      initialObjectTemp = objectTemp;  // set initial background environment temperature value
     }
-    
+
     Serial.println(abs(initialObjectTemp - prevObjectTemp));
 
-    if (abs(initialObjectTemp - prevObjectTemp) >= 2.5) {
-      if (distance < 1000 && distance !=0 && !isPressed) {
-        buzzerDuration = 300; // Set buzzer duration to normal
+    buzzerMetro.check(); // 检查蜂鸣器定时器状态
+    vibratorMetro.check(); // 检查振动器定时器状态
 
-        // ---------------- Buzzer ----------------
-        if(buzzerMetro.check()) {
-          buzzerState = !buzzerState;
-          if(buzzerState) {
-            digitalWrite(Buzzer_PIN, HIGH); // turn on the buzzer
-            buzzerStartTime = millis(); // record the start time of the buzzer
-          } else {
-            digitalWrite(Buzzer_PIN, LOW); // turn off the buzzer
-          }
+    if (abs(initialObjectTemp - prevObjectTemp) >= 1.5) {
+      Serial.println("isPressed: " + String(isPressed));
+      Serial.println("distance: " + String(distance));
+      if (distance < 500 && distance != 0 && !isPressed) {
+        buzzerDuration = 600;  // Set buzzer frequency to fast
+        Serial.println("close and not pressed");
+        vibrator(true);
+        buzzer(true);
+        leds.setColorRGB(0, 255, 0, 0);  // red color
+      } else if (distance < 1500 && distance != 0 && !isPressed) {
+        buzzerDuration = 300;  // Set buzzer frequency to slow
+        Serial.println("far and not pressed");
+        vibrator(true);
+        buzzer(true);
+        leds.setColorRGB(0, 204, 102, 0);  // yellow color
+      } else if (distance < 1000 && distance != 0 && isPressed) {
+        Serial.println("far and pressed");
+        if (distance < 500) {
+          leds.setColorRGB(0, 255, 0, 0);  // red color
+        } else {
+          leds.setColorRGB(0, 204, 102, 0);  // yellow color
         }
-        // ---------------- Vibrator ----------------
-        if(vibratorMetro.check()) {
-          vibratorState = !vibratorState;
-          digitalWrite(Vibration_PIN, vibratorState);
-        }
-        // ---------------- LED ----------------
-        leds.setColorRGB(0, 255, 0, 0); // red color
-
-        // ---------------- press button to stop playing ----------------
-        if (digitalRead(Button_PIN) == HIGH) {
-          isPressed = true;
-        }
-      } else if (distance < 1000 && distance !=0 && isPressed) {
-        buzzerDuration = longBuzzerDuration; // Set buzzer duration to shorter duration
-
-        // ---------------- Buzzer ----------------
-        if(buzzerMetro.check()) {
-          buzzerState = !buzzerState;
-          if(buzzerState) {
-            digitalWrite(Buzzer_PIN, HIGH); // turn on the buzzer
-            buzzerStartTime = millis(); // record the start time of the buzzer
-          } else {
-            digitalWrite(Buzzer_PIN, LOW); // turn off the buzzer
-          }
-        }
-        leds.setColorRGB(0, 204, 102, 0); // yellow color
-        digitalWrite(Vibration_PIN, false);
-      } else {
-        isPressed = false;
-        leds.setColorRGB(0, 0, 255, 0); //green color
-        stopPlaying();
+        vibrator(false);
+        buzzer(false);
       }
     } else {
-      isPressed = false;
-      leds.setColorRGB(0, 0, 255, 0); //green color
+      leds.setColorRGB(0, 0, 255, 0);  // green color
       stopPlaying();
+      isPressed = false;
     }
     prevObjectTemp = objectTemp;
+
   }
 
   // check if the buzzer has been on for longer than the desired duration
-  if(buzzerState && (millis() - buzzerStartTime >= buzzerDuration)) {
-    digitalWrite(Buzzer_PIN, LOW); // turn off the buzzer
-    buzzerState = false;
+  if (buzzerState && (millis() - buzzerStartTime >= buzzerDuration)) {
+    Serial.println("time" + String(millis() - buzzerStartTime));
+    stopBuzzer();
+  }
+
+  // stop buzzer and vibration if button is pressed
+  if (digitalRead(Button_PIN) == HIGH) {
+    isPressed = true;
+  }
+
+}
+
+void buzzer(bool state) {
+  buzzerState = state;
+  digitalWrite(Buzzer_PIN, buzzerState);
+  if (buzzerState) {
+    buzzerStartTime = millis();  // record the start time of the buzzer
   }
 }
 
+void stopBuzzer() {
+  digitalWrite(Buzzer_PIN, LOW);  // turn off the buzzer
+  buzzerState = false;
+}
+
+void vibrator(bool state) {
+  vibratorState = state;
+  digitalWrite(Vibration_PIN, vibratorState);
+}
+
 void stopPlaying() {
-  digitalWrite(Buzzer_PIN, false);
-  digitalWrite(Vibration_PIN, false);
+  Serial.println("in the stopPlaying");
+  stopBuzzer();
+  vibrator(false);
 }
